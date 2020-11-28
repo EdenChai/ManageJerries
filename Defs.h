efe593ca13b135d08804f8eeccfae11fc7e1859e
EdenChai <73795171+EdenChai@users.noreply.github.com>
2020-11-28 15:25:11 +0200

    Create Defs.h

---
+++ Defs.h
@@ -0,0 +1,12 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Jerry.h"
+
+typedef enum e_bool {false, true} bool;
+typedef enum e_status {success, failure} status;
+
+#endif /* DEFS_H */
